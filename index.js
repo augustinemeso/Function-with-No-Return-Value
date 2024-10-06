fdf84e8f86de320d8a3affbf3bbe346847eace11
augustinemeso <augustinemesobk@gmail.com>
2024-10-06 13:01:48 +0300

    add Function-with-No-Return-Value

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function to log user action
+function logUserAction(userAction) {
+    console.log(`User performed: ${userAction}`);
+}
+
+// Example usage
+logUserAction("logged in");
+logUserAction("viewed dashboard");
+logUserAction("logged out");
+logUserAction("Item to AddCart")
